d55e7aa148e7da3d54057e08026c422dd77279ab
Random Guy <leekhoi5319990@gmail.com>
2022-05-06 18:39:01 +0700

    04-x.4_Handling `POST` and `GET`

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -29,6 +29,7 @@ window.fetch('http://example.com/api', {headers})
 - It's a good practice to extract `async` logics into an [independent function](https://github.com/HelpMe-Pls/bookshelf-forked/blob/exercises/04-authentication/src/app.extra-1.js) then call it in inside the `useEffect` rather than defining it from withtin.
 - For better maintainability, it's highly recommended to [use early `return`s](https://github.com/HelpMe-Pls/bookshelf-forked/blob/exercises/04-authentication/src/app.extra-2.js) instead of one big `return` with multiple ternary statements in it.
 - How to handle [401 response](https://epicreact.dev/modules/build-an-epic-react-app/authentication-extra-credit-solution-03).
+- Build a `Promise` utility function which handles both [POST and GET requests](https://epicreact.dev/modules/build-an-epic-react-app/authentication-extra-credit-solution-04).
 
 ## Background
 
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,31 +1,34 @@
 import * as auth from 'auth-provider'
-
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(
+async function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {token, data, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
   }
 
-  return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
-    const data = await response.json()
-    if (response.status === 401) {
-      auth.logout()
-      window.location.assign(window.location)
-    }
-    if (response.ok) {
-      return data
-    } else {
-      return Promise.reject(data)
-    }
-  })
+  const response = await window.fetch(`${apiURL}/${endpoint}`, config)
+  if (response.status === 401) {
+    await auth.logout()
+    // refresh the page for them
+    window.location.assign(window.location)
+    return Promise.reject({message: 'Please re-authenticate.'})
+  }
+  const res = await response.json()
+  if (response.ok) {
+    return res
+  } else {
+    return Promise.reject(res)
+  }
 }
 
 export {client}
